f3027e98767aa65eeee56c56b87ea58ff78f829c
Valentina Svistunova <svistunova.vv@yandex.com>
2025-07-23 12:33:10 +0300

    Added models Added mappers Added error handler for MethodArgumentNotValidException

--- stats-server/stats-service/src/main/java/ru/practicum/statsserver/exception/ErrorHandler.java
+++ stats-server/stats-service/src/main/java/ru/practicum/statsserver/exception/ErrorHandler.java
@@ -2,16 +2,33 @@ package ru.practicum.statsserver.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Objects;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .filter(Objects::nonNull)  // Фильтруем null значения
+                .findFirst()
+                .orElse("Validation failed");
+
+        return new ErrorResponse(errorMessage);
+    }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
