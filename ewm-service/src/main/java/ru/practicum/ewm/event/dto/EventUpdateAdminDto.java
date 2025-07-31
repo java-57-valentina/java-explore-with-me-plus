dd61b16b634040d94e460db43f2cd0d3f6af5858
Valentina Svistunova <svistunova.vv@yandex.com>
2025-07-31 17:48:54 +0300

    Fixed code style

--- ewm-service/src/main/java/ru/practicum/ewm/event/dto/EventUpdateAdminDto.java
+++ ewm-service/src/main/java/ru/practicum/ewm/event/dto/EventUpdateAdminDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
