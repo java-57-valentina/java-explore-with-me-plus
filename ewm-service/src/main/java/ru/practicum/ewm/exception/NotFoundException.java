45a344076ce9293f1003fe36cab739fca2c44a01
mqrkel <meredial1997@yandex.ru>
2025-07-31 11:49:22 +0300

    Add participation request service and controller to create, retrieve, and cancel event participation requests

--- ewm-service/src/main/java/ru/practicum/ewm/exception/NotFoundException.java
+++ ewm-service/src/main/java/ru/practicum/ewm/exception/NotFoundException.java
@@ -1,7 +1,12 @@
 package ru.practicum.ewm.exception;
 
 public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
     public NotFoundException(String entity, Long id) {
-        super(entity + " with id: " + id + " not found");
+        super(String.format("Объект %s с id=%d не найден.", entity, id));
     }
 }
---
+++ ewm-service/src/main/java/ru/practicum/ewm/participation/service/ParticipationRequestService.java
@@ -0,0 +1,34 @@
+package ru.practicum.ewm.participation.service;
+
+import ru.practicum.ewm.participation.dto.ParticipationRequestDto;
+
+import java.util.List;
+
+public interface ParticipationRequestService {
+
+    /**
+     * Создает запрос на участие пользователя в событии.
+     *
+     * @param userId  ID пользователя, который хочет подать заявку
+     * @param eventId ID события, в котором хотят участвовать
+     * @return DTO созданной заявки
+     */
+    ParticipationRequestDto createRequest(Long userId, Long eventId);
+
+    /**
+     * Получает список всех заявок текущего пользователя.
+     *
+     * @param userId ID пользователя
+     * @return список DTO заявок
+     */
+    List<ParticipationRequestDto> getUserRequests(Long userId);
+
+    /**
+     * Отменяет заявку пользователя на участие.
+     *
+     * @param userId    ID пользователя
+     * @param requestId ID заявки на участие
+     * @return DTO отменённой заявки
+     */
+    ParticipationRequestDto cancelRequest(Long userId, Long requestId);
+}
