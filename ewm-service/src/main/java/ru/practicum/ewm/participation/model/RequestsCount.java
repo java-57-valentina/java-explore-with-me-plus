13e190c2f6244e25ddc73aa613847f0ed7750243
Valentina Svistunova <svistunova.vv@yandex.com>
2025-08-01 00:21:34 +0300

    Code style

--- ewm-service/src/main/java/ru/practicum/ewm/participation/model/RequestsCount.java
+++ ewm-service/src/main/java/ru/practicum/ewm/participation/model/RequestsCount.java
@@ -1,6 +1,6 @@
 package ru.practicum.ewm.participation.model;
 
 public interface RequestsCount {
-    Long getId();    // Должно совпадать с "as id" в запросе
-    Integer getCount(); // Должно совпадать с "as count"
+    Long getId();
+    Integer getCount();
 }
